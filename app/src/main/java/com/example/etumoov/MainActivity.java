f7678916534c9d6c2327764a94017dcb471275f6
KaciRayan <moussi.rayan@hotmail.com>
2021-03-12 09:20:39 +0100

    test aled

--- app/src/main/java/com/example/etumoov/MainActivity.java
+++ app/src/main/java/com/example/etumoov/MainActivity.java
@@ -14,37 +14,9 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText regNom, regPrenom, regEmail, regPassword;
-    Button regBtn, regToLoginBtn;
-
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        regNom = findViewById(R.id.reg_nom);
-        regPrenom = findViewById(R.id.reg_prenom);
-        regEmail = findViewById(R.id.reg_email);
-        regPassword = findViewById(R.id.reg_password);
-        regBtn = findViewById(R.id.reg_btn);
-        //regToLoginBtn = findViewById(R.id.reg_login_btn);
-
-        regBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nom = regNom.getText().toString();
-                rootNode = FirebaseDatabase.getInstance();
-                reference = rootNode.getReference("Utilisateur");
-                String prenom = regPrenom.getText().toString();
-                String email = regEmail.getText().toString();
-                String password = regPassword.getText().toString();
-                UserHelp helperClass = new UserHelp(nom, prenom, email, password);
-
-                reference.child(email).setValue(helperClass);
-            }
-        });
     }
 }
