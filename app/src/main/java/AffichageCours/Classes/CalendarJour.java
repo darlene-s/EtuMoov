9c3475a478b7e9b43bc43abe79f2f68bf566db11
Alexandre Hardy <hardyalexandre2000@gmail.com>
2021-03-28 16:39:47 +0200

    MAJ CalendarJour

--- app/src/main/java/AffichageCours/Classes/CalendarJour.java
+++ app/src/main/java/AffichageCours/Classes/CalendarJour.java
@@ -2,6 +2,7 @@ package AffichageCours.Classes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -11,11 +12,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import com.example.etumoov.MainActivity;
+import com.example.etumoov.IcsManager;
 import com.example.etumoov.R;
+import com.example.etumoov.ScannerActivity;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -28,15 +30,15 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
+import BD_Utilisateur.Helper_Utilisateur.DataBaseHelper;
 import BD_Utilisateur.Models_Cours.Cours;
 import biweekly.Biweekly;
 import biweekly.ICalendar;
 import biweekly.component.VEvent;
-import BD_Utilisateur.Helper_Utilisateur.*;
 
 public class CalendarJour extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView LeftArrow, RightArrow, btn_qr, btn_retour;
+    private ImageView LeftArrow, RightArrow, btn_qr, btn_refresh;
     private int datecount = 0;
     private TextView DateView;
     private ListView listView;
@@ -47,6 +49,7 @@ public class CalendarJour extends AppCompatActivity implements View.OnClickListe
     private String result = "";
     private BufferedReader reader;
     private DataBaseHelper db;
+    private IcsManager ics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,52 +57,56 @@ public class CalendarJour extends AppCompatActivity implements View.OnClickListe
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         setContentView(R.layout.activity_calendar_jour);
+        findViewByID();
 
-        this.findViewByID();
-        db = new DataBaseHelper(this);
-
-        LeftArrow.setOnClickListener(this::onClick);
-        RightArrow.setOnClickListener(this::onClick);
-        Intent intent = getIntent();
-        if(intent != null && intent.hasExtra("LienEDT")) {
-            db.deleteCours();
-            lienIntent = intent.getStringExtra("LienEDT");
-            db.insertLien(lienIntent);
-            lireLien();
-            createBD();
-        }
         Calendar calendar = Calendar.getInstance();
         Date today = calendar.getTime();
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         String todayDate = dateFormat.format(today);
         DateView.setText(todayDate);
 
-        // JEU DE DONNEES POUR TEST LE BON FONCTIONNEMENT DE L'AFFICHAGE DES COURS
+        db = new DataBaseHelper(this);
         coursList = new ArrayList<>();
-        if(!db.isLienEmpty())
-            createList();
-        mAdapter = new CoursAdapter(this, getCoursDate(coursList, getSelectedDate(datecount)));
-        listView.setAdapter(mAdapter);
+
+        createListView();
+
+        LeftArrow.setOnClickListener(this::onClick);
+        RightArrow.setOnClickListener(this::onClick);
+
         db.close();
 
         btn_qr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent edtActivity = new Intent(getApplicationContext(), MainActivity.class);
+                Intent edtActivity = new Intent(getApplicationContext(), ScannerActivity.class);
                 startActivity(edtActivity);
                 finish();
             }
         });
-        btn_retour.setOnClickListener(new View.OnClickListener() {
+        btn_refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(mainActivity);
-                finish();
+                createListView();
             }
         });
+    }
+
+    public void createListView() {
+        if(!db.isLienEmpty()) {
+            try {
+                db.deleteCours();
+                ics = new IcsManager(db.getLien(), this.getApplicationContext());
+                createList();
+                mAdapter = new CoursAdapter(this, getCoursDate(coursList, getSelectedDate(datecount)));
+                listView.setAdapter(mAdapter);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void findViewByID() {
@@ -107,26 +114,9 @@ public class CalendarJour extends AppCompatActivity implements View.OnClickListe
         RightArrow = findViewById(R.id.RightArrow);
         DateView = findViewById(R.id.textViewDate);
         btn_qr = findViewById(R.id.button_qr);
-        btn_retour = findViewById(R.id.btn_retour);
+        btn_refresh = findViewById(R.id.btn_refresh);
         listView = (ListView) findViewById(R.id.cours_list);
     }
-
-    public void lireLien(){
-        try {
-            URL url = new URL(lienIntent);
-            URLConnection conn = url.openConnection();
-            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            while ((inputLine = reader.readLine()) != null) {
-                result += inputLine + "\r\n";
-            }
-            List<ICalendar> ical = Biweekly.parse(result).all();
-            event = ical.get(0).getEvents();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     @Override
     public void onClick(View v) {
@@ -154,56 +144,8 @@ public class CalendarJour extends AppCompatActivity implements View.OnClickListe
         return demainDate;
     }
 
-    public String createDate(int i) {
-        String jour ="", mois ="", annee = "";
-            if(event.get(i).getDateStart().getValue().getDate() < 10) {
-                jour = "0";
-                jour += String.valueOf(event.get(i).getDateStart().getValue().getDate());
-            } else jour = String.valueOf(event.get(i).getDateStart().getValue().getDate());
-            if(event.get(i).getDateStart().getValue().getMonth() < 10) {
-                mois = "0";
-                mois += String.valueOf(event.get(i).getDateStart().getValue().getMonth() + 1);
-            }
-            else mois = String.valueOf(event.get(i).getDateStart().getValue().getMonth());
-            annee = String.valueOf(event.get(i).getDateStart().getValue().getYear() + 1900);
-            return jour +"/" + mois + "/" + annee;
-    }
-
-    public String createHeureDebut(int i) {
-        String heure ="", minutes ="";
-        if(event.get(i).getDateStart().getValue().getHours() < 10) {
-            heure = "0";
-            heure += String.valueOf(event.get(i).getDateStart().getValue().getHours());
-        } else heure = String.valueOf(event.get(i).getDateStart().getValue().getHours());
-        if(event.get(i).getDateStart().getValue().getMinutes() < 10) {
-            minutes = "0";
-            minutes += String.valueOf(event.get(i).getDateStart().getValue().getMinutes());
-        } else minutes = String.valueOf(event.get(i).getDateStart().getValue().getMinutes());
-        return heure + ":" + minutes;
-    }
-
-    public String createHeureFin(int i) {
-        String heure ="", minutes ="";
-        if(event.get(i).getDateEnd().getValue().getHours() < 10) {
-            heure = "0";
-            heure += String.valueOf(event.get(i).getDateEnd().getValue().getHours());
-        } else heure = String.valueOf(event.get(i).getDateEnd().getValue().getHours());
-        if(event.get(i).getDateEnd().getValue().getMinutes() < 10) {
-            minutes = "0";
-            minutes += String.valueOf(event.get(i).getDateEnd().getValue().getMinutes());
-        } else minutes = String.valueOf(event.get(i).getDateEnd().getValue().getMinutes());
-        return heure + ":" + minutes;
-    }
-
     public void createList() {
-            //coursList.add(new com.example.appmobilev2.Classes.Cours("IDK",event.get(i).getSummary().getValue(),event.get(i).getLocation().getValue(),createHeureDebut(i),createHeureFin(i),createDate(i)));
-            //coursList = db.getCours();
-    }
-
-    public void createBD() {
-        for(int i = 0; i < event.size();i++) {
-            db.insertCours("IDK",event.get(i).getSummary().getValue(),event.get(i).getLocation().getValue(),createHeureDebut(i),createHeureFin(i),createDate(i));
-        }
+        coursList = db.getCours();
     }
 
     public ArrayList<Cours> getCoursDate(ArrayList<Cours> list, String SelectedDate){
@@ -217,7 +159,7 @@ public class CalendarJour extends AppCompatActivity implements View.OnClickListe
         Collections.sort(listeCoursDay, new Comparator<Cours>() {
             @Override
             public int compare(Cours o1, Cours o2) {
-                 return o1.getDebutCours().compareTo(o2.getDebutCours());
+                return o1.getDebutCours().compareTo(o2.getDebutCours());
 
             }
         });
